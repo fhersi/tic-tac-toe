fe8b54946a85de6395944a8e5ebaa724fec101f2
F Hersi <33755306+fhersi@users.noreply.github.com>
2022-07-04 12:03:13 +0100

    add checkWin logic

--- app.js
+++ app.js
@@ -45,7 +45,6 @@ displayBoard();
 const cells = document.querySelectorAll(".cell");
 
 for (let cell of cells) {
-	console.log(cell);
 	cell.addEventListener("click", () => {
 		if (cell.innerText !== "") {
 			return;
@@ -54,3 +53,44 @@ for (let cell of cells) {
 		cell.innerText = "X";
 	});
 }
+
+const won = () => {
+	const top_row = [cells[0], cells[3], cells[6]];
+	const middle_row = [cells[1], cells[4], cells[7]];
+	const bottom_row = [cells[2], cells[5], cells[8]];
+	const left_col = [cells[0], cells[1], cells[2]];
+	const middle_col = [cells[3], cells[4], cells[5]];
+	const right_col = [cells[6], cells[7], cells[8]];
+	const diag_1 = [cells[0], cells[4], cells[8]];
+	const diag_2 = [cells[2], cells[4], cells[6]];
+
+	const checkWin = [
+		top_row,
+		middle_row,
+		bottom_row,
+		left_col,
+		middle_col,
+		right_col,
+		diag_1,
+		diag_2,
+	];
+
+	//console.log(top_row);
+
+	for (let winningCombination of checkWin) {
+		let notBlank = winningCombination[0].innerText === "";
+		if (
+			winningCombination[0].innerText === winningCombination[1].innerText &&
+			winningCombination[0].innerText === winningCombination[2].innerText &&
+			!notBlank
+		) {
+			for (let cell of winningCombination) {
+				cell.classList.add("winningCombo");
+			}
+
+			return winningCombination;
+		}
+		console.log(winningCombination);
+	}
+	return "play";
+};
